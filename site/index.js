b421ddf8b67cf9dea4f492e9e646d62655a9fd1b
devconsi <dev.consigliere@gmail.com>
2022-03-30 21:08:53 -0600

    Add - tmi setup function

--- site/index.js
+++ site/index.js
@@ -2,13 +2,27 @@
 import tmiJs from "tmi.js";
 import config from "./config/config.json"
 
-const client = new tmi.Client({
-	channels: [ config.CHANNEL ]
-});
+setupTmiClient();
 
-client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	// "Alca: Hello, World!"
-	console.log(`${tags['display-name']}: ${message}`);
-});
+
+/////////////////////////// Listen to Twitch Chat ///////////////////////////
+
+function setupTmiClient() { // Setup TMI to listen to Twitch Chat
+    const client = new tmiJs.Client({ // Setup TMI Client with the channel(s) you want to listen to
+        channels: [ config.CHANNEL ]
+    });
+  
+    client.connect(); // Connect to the channel
+  
+    client.on("message", (channel, tags, message, self) => { // Run each time a comment comes in
+      let name = tags["display-name"]; // Commenter's Name
+
+  	    console.log(`Recieved a message from: ${tags['display-name']}: ${message}`);
+
+      if (message === "!deeznuts") {
+        //blastMessage(CHAT_COMMAND, {'username': name, 'command': 'lurk'});
+        console.log("Recieved a deeznuts command")
+      }
+    });
+  }
